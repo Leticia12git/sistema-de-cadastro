c33e08f2e42b69045b7f8b7f4707c0d315cc51b8
Leticia Lima <leticia_lele.lima@hotmail.com>
2022-06-07 22:44:00 -0300

    adicionando um cosole log

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log( "Rodando o sistema de gerenciamento de jogos" )
 console.log("a aplicacao esta se conectando com banco de dados")
+// mais console log de teste:
+console.log("O paulo fez esse commit sem voce ver na aula!")
+console.log("Enviando para o GitHub!")
